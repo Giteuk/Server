3f5247c62159508fe39ce012b1e52f87bb0ab620
moonbow-ii <un9011@gmail.com>
2020-10-13 21:28:47 +0900

    plus nickname

--- user_TABLE.sql
+++ user_TABLE.sql
@@ -6,8 +6,9 @@
 CREATE TABLE `ID_TABLE` (
   `id` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
+  `N_name` varchar(30) NOT NULL,
   `name` varchar(30) NOT NULL,
-  `email` varcher(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `Pnumber` varchar(12),
   PRIMARY KEY (`id`)
 );
@@ -16,11 +17,11 @@ CREATE TABLE `ID_TABLE` (
 -- Dumping data for table `TABLE`
 --
  
-INSERT INTO `ID_TABLE` VALUES ('abc','abc123','홍길동','123abc@gmail.com','01012341234');
-INSERT INTO `ID_TABLE` VALUES ('def','def456','김길동','456def@gmail.com','01045674567');
-INSERT INTO `ID_TABLE` VALUES ('ghi','ghi789','박길동','789ghi@gmail.com','01089018901');
-INSERT INTO `ID_TABLE` VALUES ('guest','gue012','이길동','012gue@gmail.com',"01034563456");
-INSERT INTO `ID_TABLE` VALUES ('user','use345','최길동','345use@gmail.com',"01078907890");
+INSERT INTO `ID_TABLE` VALUES ('abc','abc123','홍똥','홍길동','123abc@gmail.com','01012341234');
+INSERT INTO `ID_TABLE` VALUES ('def','def456','김똥','김길동','456def@gmail.com','01045674567');
+INSERT INTO `ID_TABLE` VALUES ('ghi','ghi789','박똥','박길동','789ghi@gmail.com','01089018901');
+INSERT INTO `ID_TABLE` VALUES ('guest','gue012','이똥','이길동','012gue@gmail.com',"01034563456");
+INSERT INTO `ID_TABLE` VALUES ('user','use345','최똥','최길동','345use@gmail.com',"01078907890");
 
 -- 
 -- Table structure for table `operator_TABLE`
