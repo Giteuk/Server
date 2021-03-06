58bc1922f025d18f5d26259a4dfbf3e84d1f15ba
MacBook Pro <ghd8119@naver.com>
2021-03-06 17:37:24 +0900

    - 쿼리문 작성 시 db명.테이블 하지 않으면 쿼리가 동작하지 않던데 왜 그런지 모르겠음.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var db_config = require('../secret/database.js');
+var conn = db_config.init();
+db_config.connect(conn);
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// 회원가입
+router.post('/signUp', function(req, res, next) {
+  try{
+    var sql = `SELECT * FROM Books;`;    
+    conn.query(sql, function (err, rows, fields) {
+        if(err) res.send(err);
+        else{
+          res.send(rows);
+        } 
+    });
+  }catch(err){
+    res.send(err);
+  }
 });
 
 module.exports = router;
