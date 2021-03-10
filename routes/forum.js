0d9f65e221ae779dfe806382b089f65d715263bb
HongYeseul <ghd8119@naver.com>
2021-03-10 10:59:26 +0900

    [#7] 게시글 쓰기

--- routes/forum.js
+++ routes/forum.js
@@ -3,6 +3,21 @@ var router = express.Router();
 var db_config = require('../secret/database.js');
 var conn = db_config.init();
 
+// 게시글 쓰기
+router.post('/', (req, res, next)=>{
+  try{
+    let sql = `INSERT INTO FORUM(Title, UserNickName, Content, CreatedDate) 
+          VALUES ( '${req.body.title}', '${req.body.nickName}', '${req.body.content}', now());`; 
 
+    conn.query(sql, function (err, rows, fields) {
+        if(err) res.send(err);
+        else{
+          res.send('게시글 쓰기 성공');
+        } 
+    });
+  }catch(err){
+    res.send(err);
+  }
+})
 
 module.exports = router;
