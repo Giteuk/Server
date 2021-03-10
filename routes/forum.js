155018ddaa54558b90c699e2869c259a89101b5b
HongYeseul <ghd8119@naver.com>
2021-03-10 11:11:57 +0900

    라우터 충돌로 인한 순서 교체

--- routes/forum.js
+++ routes/forum.js
@@ -3,21 +3,40 @@ var router = express.Router();
 var db_config = require('../secret/database.js');
 var conn = db_config.init();
 
-// 게시글 쓰기
-router.post('/', (req, res, next)=>{
-  try{
-    let sql = `INSERT INTO FORUM(Title, UserNickName, Content, CreatedDate) 
-          VALUES ( '${req.body.title}', '${req.body.nickName}', '${req.body.content}', now());`; 
+router.route('/')
+  .post((req, res, next)=>{ // 게시글 쓰기
+    try{
+      let sql = `INSERT INTO FORUM(Title, UserNickName, Content, CreatedDate) 
+            VALUES ( '${req.body.title}', '${req.body.nickName}', '${req.body.content}', now());`; 
+  
+      conn.query(sql, function (err, rows, fields) {
+          if(err) res.send(err);
+          else{
+            res.send('게시글 쓰기 성공');
+          } 
+      });
+    }catch(err){
+      res.send(err);
+    }
+  })
+  .get((req, res, next)=>{ // 모든 게시글 확인
+    try{
+      let sql = `;`; 
+      res.send("모든 게시글 확인")
+      // conn.query(sql, function (err, rows, fields) {
+      //     if(err) res.send(err);
+      //     else{
+      //       res.send('게시글 쓰기 성공');
+      //     } 
+      // });
+    }catch(err){
+      res.send(err);
+    }
+  })
 
-    conn.query(sql, function (err, rows, fields) {
-        if(err) res.send(err);
-        else{
-          res.send('게시글 쓰기 성공');
-        } 
-    });
-  }catch(err){
-    res.send(err);
-  }
-})
+router.route('/:id')
+  .get((req, res, next)=>{ // 세부 게시글 확인
+    res.send("세부 게시글 확인");
+  })
 
 module.exports = router;
