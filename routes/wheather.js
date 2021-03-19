ff024366a3336849954a168044f2260c83157408
ro2130 <ro2130@naver.com>
2021-03-19 11:14:08 +0900

    날씨 값받아오기 성공 -> 추후 상의후 통신형식 설정 예정

--- routes/wheather.js
+++ routes/wheather.js
@@ -9,7 +9,6 @@ var router = express.Router();
 const options = {
   uri:"http://api.openweathermap.org/data/2.5/weather?q=Daegu&appid=0b977532e81decf779bcc5fedab20e6f&units=metric",
   json:true,
-  method: 'GET',
 }
 
 router.get('/', function(req, res, next) {
@@ -18,7 +17,16 @@ router.get('/', function(req, res, next) {
       res.send("ERROR");
     else{
       // 통신 성공 시 들어오는 곳
-      res.json(body);
+      console.log("현재온도 : "+ (body.main.temp) );
+      console.log("현재습도 : "+ body.main.humidity);
+      console.log("날씨 : "+ body.weather[0].main );
+      console.log("상세날씨설명 : "+ body.weather[0].description );
+      console.log("날씨 이미지 : "+ body.weather[0].icon );
+      console.log("바람   : "+ body.wind.speed );
+      console.log("나라   : "+ body.sys.country );
+      console.log("도시이름  : "+ body.name );
+      console.log("구름  : "+ (body.clouds.all) +"%" );  
+      res.json(body);//추후 상의후 통신형식 설정 예정
     }
   })
 });
