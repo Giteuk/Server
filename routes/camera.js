7920a498ffd87b7f22ee2389b10f0b11a4111923
HongYeseul <ghd8119@naver.com>
2021-04-29 17:56:19 +0900

    db connection camera, forum 수정

--- routes/camera.js
+++ routes/camera.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var db_config = require('../secret/database.js');
-// var conn = db_config.init();
+var mysql = require('mysql');
+var db_info = require('../secret/database.js');
 
 /* GET home page. */
 router.get('/:Farmid', function(req, res, next) {
   try{
     var sql = `SELECT * FROM CAMERA WHERE Farmid='${req.params.Farmid}';`;    
-    conn.query(sql, function (err, rows, fields) {
-        if(err) res.send(err);
+    console.log("asdf");
+    let connection = mysql.createConnection(db_info);
+    connection.connect(
+      function(err) {
+          if(err) console.error('mysql connection error : ' + err);
+          else{console.log('mysql is connected successfully!');}
+      }
+    );
+
+    connection.query(sql, function (err, rows, fields) {
+        if(err){ res.send(err); connection.end();}
         else{
           let camera1;
           let camera2;
@@ -18,6 +27,7 @@ router.get('/:Farmid', function(req, res, next) {
           camera2=rows[1].Streamlink;
           camera3=rows[2].Streamlink;
           
+          connection.end();
           res.json({camera1,camera2,camera3});
         }
     });
